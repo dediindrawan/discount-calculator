7931476accfde1f280ee9045774474c07844a1aa
Dedi Indrawan <dediindrawan812@gmail.com>
2023-04-04 13:02:20 +0700

    mengaktifkan anchor navigasi

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+// execute jumbotron button onclick
 document.querySelector('.btn-explore').addEventListener('click', () => {
     window.location.href = '#about';
 });
 
+// execute show navlink
 function openNavlink() {
     document.querySelector('.navlink').classList.toggle('show-navlink')
 };
 
+// execute button onclick
 function openKurangiDiskon() {
     window.location.href = 'kurangi-diskon.html';
 };
