277b58f9db2d4fe8f817b61b84662d7af1da7308
Dedi Indrawan <dediindrawan812@gmail.com>
2023-04-04 10:03:21 +0700

    mengubah total seluruh tampilan

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('.btn-explore').addEventListener('click', () => {
+    window.location.href = '#about';
+});
+
+function openNavlink() {
+    document.querySelector('.navlink').classList.toggle('show-navlink')
+};
+
+function openKurangiDiskon() {
+    window.location.href = '/kurangi-diskon.html';
+};
